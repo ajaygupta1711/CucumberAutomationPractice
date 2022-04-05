4b18e8e640bdd34edaaaeee9b79f14c5b6bd92f8
Ajay <ajay.gupta1711@gmail.com>
2022-04-05 21:12:01 +0530

    Cucumber Parameterization

--- src/test/java/stepDefinations/stepDefination1.java
+++ src/test/java/stepDefinations/stepDefination1.java
@@ -1,25 +1,34 @@
 package stepDefinations;
 
+import io.cucumber.java.PendingException;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.junit.Cucumber;
 
-import org.junit.runner.RunWith;
+import org.junit.runner.RunWith; 
 
 	@RunWith(Cucumber.class)
-	public class stepDefination {
+	public class stepDefination1 {
 	    @Given("^User is on NetBanking landing page$")
 	    public void user_is_on_netbanking_landing_page() throws Throwable {   
 	    	//Code to navigate to the login url
-	    	System.out.println("User is on NetBanking landing page");
+	    	
+	    	System.setProperty("webdriver.chrome.driver", "E:\\Selenium with Java\\Software Installs_IMP\\chromedriver.exe");
+	    	
 	    }
-	    @When("^User login into application with username and password$")
+/*	    @When("^User login into application with username and password$")
 	    public void user_login_into_application_with_username_and_password() throws Throwable {
 	       	//Code to login with username and password
 	    	System.out.println("User login into application with username and password");
+	    } */
+	    
+	    @When("^User login into application with \"([^\"]*)\" and \"([^\"]*)\"$")
+	    public void user_login_into_application_with_something_and_something(String strArg1, String strArg2) throws Throwable {
+	    	throw new PendingException();
 	    }
+	    
 	    @Then("^Home page is populated$")
 	    public void home_page_is_populated() throws Throwable {   
 	    	// Home page validation
